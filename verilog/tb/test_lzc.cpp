adaea0a7e59138e3fb2ad27a21bfe60d97d89deb
Taner Öksüz <taner.oeksuez@outlook.de>
2022-06-05 11:13:28 +0200

    error in source code of testbench fixed by trace option.

--- verilog/tb/test_lzc.cpp
+++ verilog/tb/test_lzc.cpp
@@ -39,6 +39,7 @@ int sc_main(int argc, char* argv[])
 
 #if VM_TRACE
   VerilatedVcdSc* dump = new VerilatedVcdSc;
+  sc_start(sc_core::SC_ZERO_TIME);
   top->trace(dump, 99);
   dump->open(dumpfile);
 #endif
