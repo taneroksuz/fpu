e731af0b959e529dc7224af07a3148e10101bf44
Taner Öksüz <taner.oeksuez@outlook.de>
2022-10-17 16:16:35 +0200

    generating test cases runs asynchronously.

--- tests/generate_test_cases.sh
+++ tests/generate_test_cases.sh
@@ -12,39 +12,41 @@ fi
 
 mkdir ${1}/test_cases
 
-${PYTHON} ${SCRIPT} "f32_mulAdd"   ${ROUND} ${1}/test_cases/ ${TESTFLOAT}
-${PYTHON} ${SCRIPT} "f32_add"      ${ROUND} ${1}/test_cases/ ${TESTFLOAT}
-${PYTHON} ${SCRIPT} "f32_sub"      ${ROUND} ${1}/test_cases/ ${TESTFLOAT}
-${PYTHON} ${SCRIPT} "f32_mul"      ${ROUND} ${1}/test_cases/ ${TESTFLOAT}
-${PYTHON} ${SCRIPT} "f32_div"      ${ROUND} ${1}/test_cases/ ${TESTFLOAT}
-${PYTHON} ${SCRIPT} "f32_sqrt"     ${ROUND} ${1}/test_cases/ ${TESTFLOAT}
-${PYTHON} ${SCRIPT} "f64_mulAdd"   ${ROUND} ${1}/test_cases/ ${TESTFLOAT}
-${PYTHON} ${SCRIPT} "f64_add"      ${ROUND} ${1}/test_cases/ ${TESTFLOAT}
-${PYTHON} ${SCRIPT} "f64_sub"      ${ROUND} ${1}/test_cases/ ${TESTFLOAT}
-${PYTHON} ${SCRIPT} "f64_mul"      ${ROUND} ${1}/test_cases/ ${TESTFLOAT}
-${PYTHON} ${SCRIPT} "f64_div"      ${ROUND} ${1}/test_cases/ ${TESTFLOAT}
-${PYTHON} ${SCRIPT} "f64_sqrt"     ${ROUND} ${1}/test_cases/ ${TESTFLOAT}
-${PYTHON} ${SCRIPT} "f32_le"       ${ROUND} ${1}/test_cases/ ${TESTFLOAT}
-${PYTHON} ${SCRIPT} "f32_lt"       ${ROUND} ${1}/test_cases/ ${TESTFLOAT}
-${PYTHON} ${SCRIPT} "f32_eq"       ${ROUND} ${1}/test_cases/ ${TESTFLOAT}
-${PYTHON} ${SCRIPT} "f64_le"       ${ROUND} ${1}/test_cases/ ${TESTFLOAT}
-${PYTHON} ${SCRIPT} "f64_lt"       ${ROUND} ${1}/test_cases/ ${TESTFLOAT}
-${PYTHON} ${SCRIPT} "f64_eq"       ${ROUND} ${1}/test_cases/ ${TESTFLOAT}
-${PYTHON} ${SCRIPT} "f32_to_f64"   ${ROUND} ${1}/test_cases/ ${TESTFLOAT}
-${PYTHON} ${SCRIPT} "f64_to_f32"   ${ROUND} ${1}/test_cases/ ${TESTFLOAT}
-${PYTHON} ${SCRIPT} "i32_to_f32"   ${ROUND} ${1}/test_cases/ ${TESTFLOAT}
-${PYTHON} ${SCRIPT} "ui32_to_f32"  ${ROUND} ${1}/test_cases/ ${TESTFLOAT}
-${PYTHON} ${SCRIPT} "i64_to_f32"   ${ROUND} ${1}/test_cases/ ${TESTFLOAT}
-${PYTHON} ${SCRIPT} "ui64_to_f32"  ${ROUND} ${1}/test_cases/ ${TESTFLOAT}
-${PYTHON} ${SCRIPT} "i32_to_f64"   ${ROUND} ${1}/test_cases/ ${TESTFLOAT}
-${PYTHON} ${SCRIPT} "ui32_to_f64"  ${ROUND} ${1}/test_cases/ ${TESTFLOAT}
-${PYTHON} ${SCRIPT} "i64_to_f64"   ${ROUND} ${1}/test_cases/ ${TESTFLOAT}
-${PYTHON} ${SCRIPT} "ui64_to_f64"  ${ROUND} ${1}/test_cases/ ${TESTFLOAT}
-${PYTHON} ${SCRIPT} "f32_to_i32"   ${ROUND} ${1}/test_cases/ ${TESTFLOAT}
-${PYTHON} ${SCRIPT} "f32_to_ui32"  ${ROUND} ${1}/test_cases/ ${TESTFLOAT}
-${PYTHON} ${SCRIPT} "f32_to_i64"   ${ROUND} ${1}/test_cases/ ${TESTFLOAT}
-${PYTHON} ${SCRIPT} "f32_to_ui64"  ${ROUND} ${1}/test_cases/ ${TESTFLOAT}
-${PYTHON} ${SCRIPT} "f64_to_i32"   ${ROUND} ${1}/test_cases/ ${TESTFLOAT}
-${PYTHON} ${SCRIPT} "f64_to_ui32"  ${ROUND} ${1}/test_cases/ ${TESTFLOAT}
-${PYTHON} ${SCRIPT} "f64_to_i64"   ${ROUND} ${1}/test_cases/ ${TESTFLOAT}
-${PYTHON} ${SCRIPT} "f64_to_ui64"  ${ROUND} ${1}/test_cases/ ${TESTFLOAT}
+${PYTHON} ${SCRIPT} "f32_mulAdd"   ${ROUND} ${1}/test_cases/ ${TESTFLOAT} &
+${PYTHON} ${SCRIPT} "f32_add"      ${ROUND} ${1}/test_cases/ ${TESTFLOAT} &
+${PYTHON} ${SCRIPT} "f32_sub"      ${ROUND} ${1}/test_cases/ ${TESTFLOAT} &
+${PYTHON} ${SCRIPT} "f32_mul"      ${ROUND} ${1}/test_cases/ ${TESTFLOAT} &
+${PYTHON} ${SCRIPT} "f32_div"      ${ROUND} ${1}/test_cases/ ${TESTFLOAT} &
+${PYTHON} ${SCRIPT} "f32_sqrt"     ${ROUND} ${1}/test_cases/ ${TESTFLOAT} &
+${PYTHON} ${SCRIPT} "f64_mulAdd"   ${ROUND} ${1}/test_cases/ ${TESTFLOAT} &
+${PYTHON} ${SCRIPT} "f64_add"      ${ROUND} ${1}/test_cases/ ${TESTFLOAT} &
+${PYTHON} ${SCRIPT} "f64_sub"      ${ROUND} ${1}/test_cases/ ${TESTFLOAT} &
+${PYTHON} ${SCRIPT} "f64_mul"      ${ROUND} ${1}/test_cases/ ${TESTFLOAT} &
+${PYTHON} ${SCRIPT} "f64_div"      ${ROUND} ${1}/test_cases/ ${TESTFLOAT} &
+${PYTHON} ${SCRIPT} "f64_sqrt"     ${ROUND} ${1}/test_cases/ ${TESTFLOAT} &
+${PYTHON} ${SCRIPT} "f32_le"       ${ROUND} ${1}/test_cases/ ${TESTFLOAT} &
+${PYTHON} ${SCRIPT} "f32_lt"       ${ROUND} ${1}/test_cases/ ${TESTFLOAT} &
+${PYTHON} ${SCRIPT} "f32_eq"       ${ROUND} ${1}/test_cases/ ${TESTFLOAT} &
+${PYTHON} ${SCRIPT} "f64_le"       ${ROUND} ${1}/test_cases/ ${TESTFLOAT} &
+${PYTHON} ${SCRIPT} "f64_lt"       ${ROUND} ${1}/test_cases/ ${TESTFLOAT} &
+${PYTHON} ${SCRIPT} "f64_eq"       ${ROUND} ${1}/test_cases/ ${TESTFLOAT} &
+${PYTHON} ${SCRIPT} "f32_to_f64"   ${ROUND} ${1}/test_cases/ ${TESTFLOAT} &
+${PYTHON} ${SCRIPT} "f64_to_f32"   ${ROUND} ${1}/test_cases/ ${TESTFLOAT} &
+${PYTHON} ${SCRIPT} "i32_to_f32"   ${ROUND} ${1}/test_cases/ ${TESTFLOAT} &
+${PYTHON} ${SCRIPT} "ui32_to_f32"  ${ROUND} ${1}/test_cases/ ${TESTFLOAT} &
+${PYTHON} ${SCRIPT} "i64_to_f32"   ${ROUND} ${1}/test_cases/ ${TESTFLOAT} &
+${PYTHON} ${SCRIPT} "ui64_to_f32"  ${ROUND} ${1}/test_cases/ ${TESTFLOAT} &
+${PYTHON} ${SCRIPT} "i32_to_f64"   ${ROUND} ${1}/test_cases/ ${TESTFLOAT} &
+${PYTHON} ${SCRIPT} "ui32_to_f64"  ${ROUND} ${1}/test_cases/ ${TESTFLOAT} &
+${PYTHON} ${SCRIPT} "i64_to_f64"   ${ROUND} ${1}/test_cases/ ${TESTFLOAT} &
+${PYTHON} ${SCRIPT} "ui64_to_f64"  ${ROUND} ${1}/test_cases/ ${TESTFLOAT} &
+${PYTHON} ${SCRIPT} "f32_to_i32"   ${ROUND} ${1}/test_cases/ ${TESTFLOAT} &
+${PYTHON} ${SCRIPT} "f32_to_ui32"  ${ROUND} ${1}/test_cases/ ${TESTFLOAT} &
+${PYTHON} ${SCRIPT} "f32_to_i64"   ${ROUND} ${1}/test_cases/ ${TESTFLOAT} &
+${PYTHON} ${SCRIPT} "f32_to_ui64"  ${ROUND} ${1}/test_cases/ ${TESTFLOAT} &
+${PYTHON} ${SCRIPT} "f64_to_i32"   ${ROUND} ${1}/test_cases/ ${TESTFLOAT} &
+${PYTHON} ${SCRIPT} "f64_to_ui32"  ${ROUND} ${1}/test_cases/ ${TESTFLOAT} &
+${PYTHON} ${SCRIPT} "f64_to_i64"   ${ROUND} ${1}/test_cases/ ${TESTFLOAT} &
+${PYTHON} ${SCRIPT} "f64_to_ui64"  ${ROUND} ${1}/test_cases/ ${TESTFLOAT} &
+
+wait
