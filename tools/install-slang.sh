eb194b059d11194e6572640b902d90873513d78b
Taner Öksüz <taner.oeksuez@outlook.de>
2024-02-21 17:59:46 +0100

    script updated.

--- tools/install-slang.sh
+++ tools/install-slang.sh
@@ -1,43 +1,7 @@
 #!/bin/bash
 set -e
 
-sudo apt-get -y install git build-essential libssl-dev \
-                        libmpfrc++-dev libmpc-dev libgmp-dev \
-                        gcc-multilib
-
-if [ -d "CMake" ]; then
-  rm -rf CMake
-fi
-
-git clone https://github.com/Kitware/CMake.git
-
-cd CMake
-
-git checkout release
-
-./configure --prefix=/usr/local
-
-make -j$(nproc)
-sudo make install
-
-cd -
-
-if [ -d "gcc" ]; then
-  rm -rf gcc
-fi
-
-git clone https://gcc.gnu.org/git/gcc.git
-
-cd gcc
-
-git checkout remotes/origin/releases/gcc-13
-
-./configure --prefix=/usr/local --enable-languages=c,c++
-
-make -j$(nproc)
-sudo make install
-
-cd -
+sudo apt-get -y install git build-essential cmake
 
 if [ -d "slang" ]; then
   rm -rf slang
